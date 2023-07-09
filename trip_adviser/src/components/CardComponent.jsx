9130a85f265e85eebc05009a0554be321da565f3
Siva Charan <gurramsivacharan@gmail.com>
2023-07-09 22:27:03 +0530

    refractor: card component updated

--- trip_adviser/src/components/CardComponent.jsx
+++ trip_adviser/src/components/CardComponent.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import {MdLocationOn} from "react-icons/md"
-import "./CardComponent.css"
+import React from 'react';
+import { MdLocationOn } from 'react-icons/md';
+import './CardComponent.css';
 
-export const CardComponent = ({item}) => {
+export const CardComponent = ({ item }) => {
   return (
-    <div>
-        <div>
-            <img src={item.image} alt={item.id} width="300px" />
-        </div>
-        <div>
-            <p><MdLocationOn /> {item.name}</p>
-        </div>
+    <div className="card-container">
+      <div className="image-container">
+        <img src={item.image} alt={item.id} width="250px" height="250px" />
+      </div>
+      <div className="name-container">
+        <p>
+          <MdLocationOn /> {item.name}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
